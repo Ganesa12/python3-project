29c6203b689de5af26645b8749457fa48a5f8f46
ganesa <164351696+Ganesa12@users.noreply.github.com>
2024-12-05 09:59:04 +0700

    Create perhitunaganfcsoal.py

---
+++ perhitunaganfcsoal.py
@@ -0,0 +1,15 @@
+print("penghitungan foto copy")
+print("konversi total sudah di tambah 100 ribu")
+a = int(input("masukan jumlah lembar biasa : "))
+hasila = (a*200 + 50000) / 200
+hasilb = a*200 + 50000
+b = int(input("masukan jumlah lembar bolak balik : "))
+hasilc = (b*220 + 50000) / 220 * 2
+hasild = b*220 + 50000
+print('='*60)
+print("jumlah lembar yang di hasilkan fc biasa = ", hasila)
+print("total harga fc biasa= ", hasilb)
+print('='*60)
+print("jumlah Halaman yang di hasilkan fc bolak balik = ", hasilc)
+print("total harga fc bolak balik= ", hasild)
+print('='*60)
